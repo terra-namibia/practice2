83ec4aa9ba4ccc1e63fe7816947cdccdc200b24e
terra-namibia <tera9319@yahoo.co.jp>
2022-03-28 15:04:40 +0900

    add Animated Widget

--- lib/main.dart
+++ lib/main.dart
@@ -34,6 +34,8 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   String _type = "偶数";
+  bool selected = false;
+  bool _lightIsOn = false;
 
   void _incrementCounter() {
     setState(() {
@@ -77,11 +79,6 @@ class _MyHomePageState extends State<MyHomePage> {
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               Icon(
-                Icons.favorite,
-                color: Colors.pink,
-                size: 24.0,
-              ),
-              Icon(
                 Icons.audiotrack,
                 color: Colors.green,
                 size: 30.0,
@@ -93,6 +90,117 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ],
           ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              AnimatedSwitcher(
+                  duration: Duration(seconds: 1),
+                  child: _lightIsOn
+                      ? Icon(Icons.favorite, color: Colors.pink)
+                      : Text("なにもない")),
+              AnimatedContainer(
+                  duration: const Duration(seconds: 1),
+                  width: _lightIsOn ? 15 : 5,
+                  height: _lightIsOn ? 5 : 15,
+                  padding: _lightIsOn ? EdgeInsets.all(0) : EdgeInsets.all(5),
+                  margin: _lightIsOn ? EdgeInsets.all(0) : EdgeInsets.all(5),
+                  color: _lightIsOn ? Colors.blue : Colors.grey),
+              AnimatedOpacity(
+                  opacity: _lightIsOn ? 1.0 : 0.0,
+                  duration: Duration(seconds: 1),
+                  child: Text(
+                    "浮かぶ文字",
+                    style: Theme.of(context).textTheme.headline5,
+                  )),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Icon(
+              Icons.lightbulb_outline,
+              color: _lightIsOn ? Colors.yellow.shade600 : Colors.black,
+              size: 60,
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _lightIsOn = !_lightIsOn;
+                  });
+                },
+                child: Container(
+                  color: Colors.yellow.shade600,
+                  padding: const EdgeInsets.all(8),
+                  margin: const EdgeInsets.all(4),
+                  child: Text(_lightIsOn ? '押せる OFF' : '押せる ON'),
+                ),
+              ),
+              Container(
+                color: Color.fromARGB(255, 238, 227, 177),
+                padding: const EdgeInsets.all(8),
+                margin: const EdgeInsets.all(4),
+                child: Text(_lightIsOn ? '押せない OFF' : '押せない ON'),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Container(
+                color: Color.fromARGB(255, 238, 227, 177),
+                padding: const EdgeInsets.all(8),
+                margin: const EdgeInsets.all(4),
+                child: Text(_lightIsOn ? '押せない OFF' : '押せない ON'),
+              ),
+            ],
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            margin: const EdgeInsets.all(4),
+            child: AnimatedAlign(
+                duration: const Duration(seconds: 1),
+                alignment:
+                    _lightIsOn ? Alignment.topLeft : Alignment.bottomRight,
+                child: SizedBox(
+                    width: 10,
+                    height: 10,
+                    child: Container(color: Colors.green))),
+          ),
+          Container(
+            margin: const EdgeInsets.all(4),
+            child: AnimatedSize(
+                duration: const Duration(seconds: 1),
+                alignment:
+                    _lightIsOn ? Alignment.topLeft : Alignment.bottomRight,
+                child: SizedBox(
+                    width: _lightIsOn ? 5 : 15,
+                    height: _lightIsOn ? 5 : 15,
+                    child: Container(color: Colors.purple))),
+          ),
+          GestureDetector(
+            onTap: () {
+              setState(() {
+                selected = !selected;
+              });
+            },
+            child: Center(
+              child: Container(
+                width: 150.0,
+                height: 150.0,
+                color: Color.fromARGB(255, 250, 121, 112),
+                child: AnimatedAlign(
+                  alignment:
+                      selected ? Alignment.topRight : Alignment.bottomLeft,
+                  duration: const Duration(seconds: 1),
+                  curve: Curves.fastOutSlowIn,
+                  child: const FlutterLogo(size: 30.0),
+                ),
+              ),
+            ),
+          ),
           TextButton(
               onPressed: () => {
                     Navigator.of(context)
@@ -100,7 +208,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       return TestPage2();
                     }))
                   },
-              child: Text("進む", style: TextStyle(fontSize: 80))),
+              child: Text("進む", style: TextStyle(fontSize: 30))),
         ],
       ),
       floatingActionButton: FloatingActionButton(
