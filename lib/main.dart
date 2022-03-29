3d9d34661cffbe8d7d512170d57f3258a462e690
terra-namibia <tera9319@yahoo.co.jp>
2022-03-29 10:34:14 +0900

    add SingleChildScrollView to scroll and show large contents

--- lib/main.dart
+++ lib/main.dart
@@ -107,184 +107,186 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      body: Column(
-        children: [
-          const Text("data1"),
-          const Text("data2"),
-          Text(
-            '$_counter',
-            style: Theme.of(context).textTheme.headline4,
-          ),
-          if (_counter % 2 == 0)
-            const Text('偶数です',
-                style: TextStyle(fontSize: 20, color: Colors.red)),
-          if (_counter % 2 == 1)
-            const Text('奇数です',
-                style: TextStyle(fontSize: 20, color: Colors.red)),
-          Text('$_type',
-              style: const TextStyle(fontSize: 20, color: Colors.red)),
-          TextButton(
-            onPressed: () => {print("updateボタンがおされたよ")},
-            child: const Text("update"),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: const [
-              Icon(
-                Icons.audiotrack,
-                color: Colors.green,
-                size: 30.0,
-              ),
-              Icon(
-                Icons.beach_access,
-                color: Colors.blue,
-                size: 36.0,
-              ),
-              Icon(FontAwesomeIcons.globe, color: Colors.cyan)
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              AnimatedSwitcher(
-                  duration: const Duration(seconds: 1),
-                  child: _lightIsOn
-                      ? const Icon(Icons.favorite, color: Colors.pink)
-                      : const Text("なにもない")),
-              AnimatedContainer(
-                  duration: const Duration(seconds: 1),
-                  width: _lightIsOn ? 15 : 5,
-                  height: _lightIsOn ? 5 : 15,
-                  padding: _lightIsOn
-                      ? const EdgeInsets.all(0)
-                      : const EdgeInsets.all(5),
-                  margin: _lightIsOn
-                      ? const EdgeInsets.all(0)
-                      : const EdgeInsets.all(5),
-                  color: _lightIsOn ? Colors.blue : Colors.grey),
-              AnimatedOpacity(
-                  opacity: _lightIsOn ? 1.0 : 0.0,
-                  duration: const Duration(seconds: 1),
-                  child: Text(
-                    "浮かぶ文字",
-                    style: Theme.of(context).textTheme.headline5,
-                  )),
-            ],
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Icon(
-              Icons.lightbulb_outline,
-              color: _lightIsOn ? Colors.yellow.shade600 : Colors.black,
-              size: 60,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const Text("data1"),
+            const Text("data2"),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              GestureDetector(
-                onTap: () {
-                  setState(() {
-                    _lightIsOn = !_lightIsOn;
-                  });
-                },
-                child: Container(
-                  color: Colors.yellow.shade600,
+            if (_counter % 2 == 0)
+              const Text('偶数です',
+                  style: TextStyle(fontSize: 20, color: Colors.red)),
+            if (_counter % 2 == 1)
+              const Text('奇数です',
+                  style: TextStyle(fontSize: 20, color: Colors.red)),
+            Text('$_type',
+                style: const TextStyle(fontSize: 20, color: Colors.red)),
+            TextButton(
+              onPressed: () => {print("updateボタンがおされたよ")},
+              child: const Text("update"),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: const [
+                Icon(
+                  Icons.audiotrack,
+                  color: Colors.green,
+                  size: 30.0,
+                ),
+                Icon(
+                  Icons.beach_access,
+                  color: Colors.blue,
+                  size: 36.0,
+                ),
+                Icon(FontAwesomeIcons.globe, color: Colors.cyan)
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                AnimatedSwitcher(
+                    duration: const Duration(seconds: 1),
+                    child: _lightIsOn
+                        ? const Icon(Icons.favorite, color: Colors.pink)
+                        : const Text("なにもない")),
+                AnimatedContainer(
+                    duration: const Duration(seconds: 1),
+                    width: _lightIsOn ? 15 : 5,
+                    height: _lightIsOn ? 5 : 15,
+                    padding: _lightIsOn
+                        ? const EdgeInsets.all(0)
+                        : const EdgeInsets.all(5),
+                    margin: _lightIsOn
+                        ? const EdgeInsets.all(0)
+                        : const EdgeInsets.all(5),
+                    color: _lightIsOn ? Colors.blue : Colors.grey),
+                AnimatedOpacity(
+                    opacity: _lightIsOn ? 1.0 : 0.0,
+                    duration: const Duration(seconds: 1),
+                    child: Text(
+                      "浮かぶ文字",
+                      style: Theme.of(context).textTheme.headline5,
+                    )),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Icon(
+                Icons.lightbulb_outline,
+                color: _lightIsOn ? Colors.yellow.shade600 : Colors.black,
+                size: 60,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      _lightIsOn = !_lightIsOn;
+                    });
+                  },
+                  child: Container(
+                    color: Colors.yellow.shade600,
+                    padding: const EdgeInsets.all(8),
+                    margin: const EdgeInsets.all(4),
+                    child: Text(_lightIsOn ? '押せる OFF' : '押せる ON'),
+                  ),
+                ),
+                Container(
+                  color: const Color.fromARGB(255, 238, 227, 177),
                   padding: const EdgeInsets.all(8),
                   margin: const EdgeInsets.all(4),
-                  child: Text(_lightIsOn ? '押せる OFF' : '押せる ON'),
+                  child: Text(_lightIsOn ? '押せない OFF' : '押せない ON'),
                 ),
-              ),
-              Container(
-                color: const Color.fromARGB(255, 238, 227, 177),
-                padding: const EdgeInsets.all(8),
-                margin: const EdgeInsets.all(4),
-                child: Text(_lightIsOn ? '押せない OFF' : '押せない ON'),
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              Container(
-                color: const Color.fromARGB(255, 238, 227, 177),
-                padding: const EdgeInsets.all(8),
-                margin: const EdgeInsets.all(4),
-                child: Text(_lightIsOn ? '押せない OFF' : '押せない ON'),
-              ),
-            ],
-          ),
-          AnimatedSwitcher(
-              duration: const Duration(seconds: 1),
-              child: _lightIsOn
-                  ? Center(
-                      child: Container(
-                          padding: EdgeInsets.all(10.0),
-                          child: SizedBox(
-                              width: 15,
-                              height: 15,
-                              child: CircularProgressIndicator(
-                                strokeWidth: 3.0,
-                              ))))
-                  : const Text("")),
-          Container(
-            padding: const EdgeInsets.all(8),
-            margin: const EdgeInsets.all(4),
-            child: AnimatedAlign(
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Container(
+                  color: const Color.fromARGB(255, 238, 227, 177),
+                  padding: const EdgeInsets.all(8),
+                  margin: const EdgeInsets.all(4),
+                  child: Text(_lightIsOn ? '押せない OFF' : '押せない ON'),
+                ),
+              ],
+            ),
+            AnimatedSwitcher(
                 duration: const Duration(seconds: 1),
-                alignment:
-                    _lightIsOn ? Alignment.bottomRight : Alignment.topLeft,
-                child: SizedBox(
-                    width: 10,
-                    height: 10,
-                    child: Container(color: Colors.green))),
-          ),
-          Container(
-            margin: const EdgeInsets.all(4),
-            child: AnimatedSize(
-                duration: const Duration(seconds: 1),
-                alignment:
-                    _lightIsOn ? Alignment.topLeft : Alignment.bottomRight,
-                child: SizedBox(
-                    width: _lightIsOn ? 5 : 15,
-                    height: _lightIsOn ? 5 : 15,
-                    child: Container(color: Colors.purple))),
-          ),
-          GestureDetector(
-            onTap: () {
-              setState(() {
-                selected = !selected;
-              });
-            },
-            child: Center(
-              child: Container(
-                width: 150.0,
-                height: 150.0,
-                color: const Color.fromARGB(255, 250, 121, 112),
-                child: AnimatedAlign(
-                  alignment:
-                      selected ? Alignment.topRight : Alignment.bottomLeft,
+                child: _lightIsOn
+                    ? Center(
+                        child: Container(
+                            padding: const EdgeInsets.all(10.0),
+                            child: const SizedBox(
+                                width: 15,
+                                height: 15,
+                                child: CircularProgressIndicator(
+                                  strokeWidth: 3.0,
+                                ))))
+                    : const Text("")),
+            Container(
+              padding: const EdgeInsets.all(8),
+              margin: const EdgeInsets.all(4),
+              child: AnimatedAlign(
                   duration: const Duration(seconds: 1),
-                  curve: Curves.fastOutSlowIn,
-                  child: const FlutterLogo(size: 30.0),
+                  alignment:
+                      _lightIsOn ? Alignment.bottomRight : Alignment.topLeft,
+                  child: SizedBox(
+                      width: 16,
+                      height: 16,
+                      child: Container(color: Colors.green))),
+            ),
+            Container(
+              margin: const EdgeInsets.all(14),
+              child: AnimatedSize(
+                  duration: const Duration(seconds: 1),
+                  alignment:
+                      _lightIsOn ? Alignment.topLeft : Alignment.bottomRight,
+                  child: SizedBox(
+                      width: _lightIsOn ? 84 : 16,
+                      height: _lightIsOn ? 84 : 16,
+                      child: Container(color: Colors.purple))),
+            ),
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  selected = !selected;
+                });
+              },
+              child: Center(
+                child: Container(
+                  width: 150.0,
+                  height: 150.0,
+                  color: const Color.fromARGB(255, 250, 121, 112),
+                  child: AnimatedAlign(
+                    alignment:
+                        selected ? Alignment.topRight : Alignment.bottomLeft,
+                    duration: const Duration(seconds: 1),
+                    curve: Curves.fastOutSlowIn,
+                    child: const FlutterLogo(size: 30.0),
+                  ),
                 ),
               ),
             ),
-          ),
-          ElevatedButton(
-            style: raisedButtonStyle,
-            onPressed: _launchURL,
-            child: const Text('Show Flutter homepage'),
-          ),
-          TextButton(
-              onPressed: () => {
-                    Navigator.of(context)
-                        .push(MaterialPageRoute(builder: (context) {
-                      return TestPage2();
-                    }))
-                  },
-              child: const Text("進む", style: TextStyle(fontSize: 30))),
-        ],
+            ElevatedButton(
+              style: raisedButtonStyle,
+              onPressed: _launchURL,
+              child: const Text('Show Flutter homepage'),
+            ),
+            TextButton(
+                onPressed: () => {
+                      Navigator.of(context)
+                          .push(MaterialPageRoute(builder: (context) {
+                        return TestPage2();
+                      }))
+                    },
+                child: const Text("進む", style: TextStyle(fontSize: 30))),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
           onPressed: () =>
