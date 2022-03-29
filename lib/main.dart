74c40d20a93f5841b21385058c09b7503a271f45
terra-namibia <tera9319@yahoo.co.jp>
2022-03-29 10:27:22 +0900

    modify Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -76,7 +76,37 @@ class _MyHomePageState extends State<MyHomePage> {
           Text("初めてのタイトル"),
         ]),
       ),
-      drawer: const Drawer(child: Center(child: Text("Drawer"))),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Drawer Header',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.message),
+              title: Text('Messages'),
+            ),
+            ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text('Profile'),
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+            ),
+          ],
+        ),
+      ),
       body: Column(
         children: [
           const Text("data1"),
